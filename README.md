8307310feb0503969d8ef8be0da7b4f66d194c95
Kehaar-II <kappleravec2p@gmail.com>
2024-04-06 20:05:55 +0200

    ver. 0

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-### Todo
+#  gradient-bg-remover
+TODO
+
+## Requirements
+ezImageGrid requires the followings to work:
+- Python 3
+- Pillow
+
+written in Python 3.11.7
---
+++ remove_bg.py
@@ -0,0 +1,14 @@
+#!/bin/python3
+
+from PIL import Image
+import sys
+import os
+from math import ceil
+
+def main():
+    '''void -> int'''
+    Image.open("").save("")
+
+    return 0
+
+exit(main())
